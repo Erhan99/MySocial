9485b0d33aad86deedd42c66c9840ac5dd96fe83
Erhan99 <146257442+Erhan99@users.noreply.github.com>
2025-04-11 15:13:14 +0200

    setup project

---
+++ MySocial/MySocial.WebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MySocial.WebUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
